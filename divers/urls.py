397e1a194550e06f714c02f55b8cad02503bb252
StevenDelval <asteven59220@free.fr>
2022-11-30 15:49:04 +0100

    login signup

--- divers/urls.py
+++ divers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("" , home_view , name="home")
+    path("" , home_view , name="home"),
+    path("signup/",UserCreateView.as_view(),name="signup")
 ]
--- divers/views.py
+++ divers/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import User
+
 
 # Create your views here.
 def home_view(request):
     return render(request, "index.html")
+
+class UserCreateView(CreateView):
+    model = User
+    success_url = ""
+    template_name = "registration/signup.html"
